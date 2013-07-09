df2caf7f559a91a921426e930c4ab7d2ed8b158c
Miguel Angel Mesas <miguel-angel.mesas@gefco.es>
2013-07-09 23:02:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readFileSync('./index.html');   
+  var buf = new Buffer(fs.readFileSync('./index.html'));   
   response.send(buf.toString());
 });
 
